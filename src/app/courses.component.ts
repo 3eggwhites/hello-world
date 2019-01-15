6df413ce5871540d1a426e7581af23cf185197d5
Ayan Pal <ayangitrepo@gmail.com>
2019-01-16 02:16:47 +0530

    service poc

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,26 @@
+import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `<h2>{{getTitle()}}</h2>
+    <ul>
+    <li *ngFor="let course of courses">
+    {{course}}
+    </li>
+    </ul>
+    `
 })
 
 export class CoursesComponent{
+    title = 'List of courses'
+    courses;
+
+    constructor(coursesSvc: CoursesService){
+        this.courses = coursesSvc.getCourses();
+    }
+
+    getTitle(){
+        return this.title;
+    }
 }
